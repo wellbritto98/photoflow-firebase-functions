dc4e40135bbfbe417330989ae7bd5b074ae1131d
João Victor Linhares <joao.linhares.dev@gmail.com>
2025-05-17 14:07:20 -0300

    refactor image processing to handle file uploads and improve denoising response

--- functions/main.py
+++ functions/main.py
@@ -9,22 +9,30 @@ initialize_app()
 @https_fn.on_request()
 def denoise_image(req: https_fn.Request) -> https_fn.Response:
     try:
-        data = req.get_json()
-        image_b64 = data.get("image")
-        if not image_b64:
-            return ("No image provided", 400)
+        # Verifica se o arquivo foi enviado
+        if 'img' not in req.files:
+            return ("No image file provided", 400)
 
-        image_data = base64.b64decode(image_b64)
-        nparr = np.frombuffer(image_data, np.uint8)
+        file = req.files['img']
+        in_memory_file = file.read()
+        nparr = np.frombuffer(in_memory_file, np.uint8)
         img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
 
         # Denoising
-        denoised = cv2.fastNlMeansDenoisingColored(img, None, 10, 10, 7, 21)
-        _, buffer = cv2.imencode(".jpg", denoised)
-        denoised_b64 = base64.b64encode(buffer).decode("utf-8")
+        denoised = cv2.fastNlMeansDenoisingColored(img, None, 10, 8, 7, 21)
 
+        # Sharpening filter
+        kernel = np.array([[0, -1, 0],
+                           [-1, 5, -1],
+                           [0, -1, 0]])
+        sharpened = cv2.filter2D(denoised, -1, kernel)
+
+        _, buffer = cv2.imencode(".jpg", sharpened)
+
+        # Retorna como base64 (pois não há send_file)
+        img_b64 = base64.b64encode(buffer).decode("utf-8")
         return {
-            "denoised_image": denoised_b64
+            "denoised_image": img_b64
         }
     except Exception as e:
         return ({
